c350cc1e7b987d7b99a24a7d478f24f1da6ddced
Dmitry Medvinsky <dmitry.medvinsky@e-legion.com>
2018-05-30 16:23:02 +0300

    Use strings instead of symbols for settings names

--- lib/helpers/checker.rb
+++ lib/helpers/checker.rb
@@ -1,7 +1,7 @@
 module Helpers
   module Checker
     def allowed_domain_for? email
-      allowed_domains = Setting.plugin_redmine_omniauth_google[:allowed_domains]
+      allowed_domains = Setting.plugin_redmine_omniauth_google["allowed_domains"]
       return unless allowed_domains
       allowed_domains = allowed_domains.split
       return true if allowed_domains.empty?
@@ -9,13 +9,13 @@ module Helpers
     end
 
     def trusted_domain?(hd)
-      domains = Setting.plugin_redmine_omniauth_google[:trusted_domains].try(:split)
+      domains = Setting.plugin_redmine_omniauth_google["trusted_domains"].try(:split)
       return false if domains.blank?
       domains.index(hd)
     end
 
     def user_mapped_groups(hd)
-      setting = Setting.plugin_redmine_omniauth_google[:group_mapping]
+      setting = Setting.plugin_redmine_omniauth_google["group_mapping"]
       return [] if setting.blank?
       mapping = Hash[setting.split.map { |x| x.split(":") }]
       ids = mapping[hd].try(:split, ",") || []
